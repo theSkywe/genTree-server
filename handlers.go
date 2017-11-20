a572fe2c34ed951b930f2d4a52f572e662bc1699
Skywe <orlovsky.we.oleg@gmail.com>
2017-11-20 17:08:58 +0200

    node deleting now works fine

--- handlers.go
+++ handlers.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func getTreeHandler(w http.ResponseWriter, r *http.Request) {
@@ -34,14 +37,15 @@ func addNodeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func deleteNodeHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 	db, err := newOpen("mysql", dbCOnnectionString)
 	checkErr(err)
 	defer db.Close()
 
-	var n Node
-	_ = json.NewDecoder(r.Body).Decode(&n)
+	nodeID, err := strconv.Atoi(params["id"])
+	checkErr(err)
 
-	err = db.deleteNodeFromDB(n.ID)
+	err = db.deleteNodeFromDB(nodeID)
 	checkErr(err)
 
 	nodes, err = db.getAllNodesFromDB()
